95673bf759cc9e35984efba5e6982a58c8a6723a
lance liu <lance_liu@163.com>
2020-03-21 14:48:38 +0800

    进入home组件token验证

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -102,7 +102,15 @@
 </template>
 
 <script>
-export default {};
+
+export default {
+    beforeCreate(){
+        const token = localStorage.getItem('token')
+        if(!token){
+            this.$router.push({name:'login'})
+        }
+    }
+};
 </script>
 
 <style>
