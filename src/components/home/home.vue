fa42bc92bd1ffdf57664b5960517049f689ae502
lance liu <lance_liu@163.com>
2020-03-21 14:06:47 +0800

    引入导航组件调整样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,129 @@
 <template>
-    <div>home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">
+            <img src="../../assets/logo.png" alt="无法显示图片" />
+          </div>
+        </el-col>
+        <el-col :span="20" class="middle">
+          <div class="grid-content bg-purple-light">
+            <h3>电商后台管理系统</h3>
+          </div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">
+            <a class="login-out" href="#">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <el-menu class="el-menu-vertical-demo">
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
+.middle {
+  text-align: center;
+}
+.login-out {
+  text-decoration: none;
+  line-height: 60px;
+}
 </style>
