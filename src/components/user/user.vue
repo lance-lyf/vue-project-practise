d8f166997f819f15f2afa49523b67e69a21ca7d1
lance liu <lance_liu@163.com>
2020-03-23 05:53:50 +0800

    完成用户功能的渲染

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -15,16 +15,27 @@
     </el-row>
     <el-table :data="tableData" style="width: 100%">
       <el-table-column type="index" label="#" width="180"></el-table-column>
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="邮箱"></el-table-column>
-      <el-table-column prop="address" label="电话"></el-table-column>
-      <el-table-column prop="address" label="创建时间"></el-table-column>
-      <el-table-column prop="address" label="操作"></el-table-column>
+      <el-table-column prop="role_name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
+      <el-table-column prop="mobile" label="电话"></el-table-column>
+      <el-table-column prop="create_time" label="创建时间">
+        <template slot-scope="scope">{{scope.row.create_time | fmtdate}}</template>
+      </el-table-column>
+      <el-table-column prop="mg_state" label="用户状态">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="操作">
+        <template>
+          <el-button size="mini" type="primary" plain="true" icon="el-icon-edit" circle></el-button>
+          <el-button size="mini" type="success" plain="true" icon="el-icon-check" circle></el-button>
+          <el-button size="mini" type="danger" plain="true" icon="el-icon-delete" circle></el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
-
 <script>
 export default {
   data() {
@@ -32,20 +43,42 @@ export default {
       query: "",
       pagenum: 1,
       pagesize: 2,
-      tableData: [
-       
-      ]
+      tableData: [],
+      total: -1
+      //分页
     };
   },
-  created(){
-      this.getUserList()
+  created() {
+    this.getUserList();
   },
   methods: {
-      async getUserList(){
-          const AUTH_TOKEN = localStorage.getItem('token')
-          this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
-          const res = await this.$http.get('users?query='+this.query+'&pagenum='+this.pagenum+'&pagesize='+this.pagesize)
+    async getUserList() {
+      const AUTH_TOKEN = localStorage.getItem("token");
+      this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+      const res = await this.$http.get(
+        "users?query=" +
+          this.query +
+          "&pagenum=" +
+          this.pagenum +
+          "&pagesize=" +
+          this.pagesize
+      );
+      const {
+        meta: { status, msg },
+        data: { users, total }
+      } = res.data;
+      if (status === 200) {
+        //1.给表格数据赋值
+        this.tableData = users;
+        //2.给total赋值
+        this.total = total;
+        //3.提示
+        this.$message.success(msg);
+        //提示
+      } else {
+        this.$message.warning(msg);
       }
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import MySeverHttp from './plugins/http.js'
+import Moment from 'moment'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/reset.css'
 Vue.use(ElementUI)
 Vue.use(MySeverHttp)
 Vue.config.productionTip = false// 生产环境 调试不输出信息
 /* eslint-disable no-new */
+Vue.filter('fmtdate',v=>{
+  return Moment(v).format('YYYY-MM-DD')
+})
 new Vue({
   el: '#app',
   router,
