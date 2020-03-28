d057c63f747cb873c97cf0e16fddda6050494e7a
lance liu <lance_liu@163.com>
2020-03-28 21:32:55 +0800

    动态侧边栏及导航守卫

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -23,76 +23,15 @@
       <el-aside width="200px" class="aside">
         <el-menu class="el-menu-vertical-demo" :router="true">
           <!-- 1 -->
-          <el-submenu index="1">
+          <el-submenu :index="''+item1.order" v-for="(item1, i) in menus" :key="i">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{item1.authName}}</span>
             </template>
-            <el-menu-item index="users">
+            <el-menu-item :index="item2.path" v-for="(item2,i) in item1.children" :key="i">
               <i class="el-icon-location"></i>
-              <span>用户列表</span>
+              <span>{{item2.authName}}</span>
             </el-menu-item>
-            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
-          </el-submenu>
-          <!-- 2 -->
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="role">
-              <i class="el-icon-location"></i>
-              <span>角色列表</span>
-            </el-menu-item>
-            <el-menu-item index="rights">
-              <i class="el-icon-location"></i>
-              <span>权限列表</span>
-            </el-menu-item>
-            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
-          </el-submenu>
-          <!-- 3 -->
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item index="1-1">
-              <i class="el-icon-location"></i>
-              <span>商品列表</span>
-            </el-menu-item>
-            <el-menu-item index="1-1">
-              <i class="el-icon-location"></i>
-              <span>分类参数</span>
-            </el-menu-item>
-            <el-menu-item index="1-1">
-              <i class="el-icon-location"></i>
-              <span>商品分类</span>
-            </el-menu-item>
-            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
-          </el-submenu>
-          <!-- 4 -->
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item index="1-1">
-              <i class="el-icon-location"></i>
-              <span>订单列表</span>
-            </el-menu-item>
-            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
-          </el-submenu>
-          <!-- 5 -->
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item index="1-1">
-              <i class="el-icon-location"></i>
-              <span>数据报表</span>
-            </el-menu-item>
-            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -107,11 +46,13 @@
 
 <script>
 export default {
-  beforeCreate() {
-    const token = localStorage.getItem("token")
-    if (!token) {
-      this.$router.push({ name: "login" })
+  data(){
+    return {
+      menus: []
     }
+  },  
+  created(){
+    this.getMenus();
   },
   methods: {
     handleSignout() {
@@ -121,6 +62,11 @@ export default {
         type: "success"
       })
       this.$router.push({name:'login'})
+    },
+    async getMenus(){
+      const res = await this.$http.get('menus')
+      this.menus = res.data.data
+      console.log(this.menus);
     }
   }
 };
--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,11 @@ import Login from '../components/login/login.vue'
 import Home from '../components/home/home.vue'
 import users from '../components/user/user.vue'
 import right from '../components/rights/right.vue'
-import role from '../components/rights/role.vue'
+import roles from '../components/rights/role.vue'
+import { Message } from 'element-ui'
 Vue.use(Router)
-export default new Router({
+
+const router = new Router({
   routes: [
     {
       name: 'login',
@@ -19,8 +21,28 @@ export default new Router({
       children:[
         {name:'users',path:'/users',component:users},
         {name:'right',path:'/rights',component:right},
-        {name:'role',path:'/role',component:role}
+        {name:'role',path:'/roles',component:roles}
       ]
     }
   ]
 })
+router.beforeEach((to,from,next)=>{
+  //到哪去的路由
+  console.log(to);
+  //从来来的路由
+  console.log(from);
+  if(to.path=="/login"){
+    next();
+  }else{
+    const token = localStorage.getItem("token")
+    if (!token) {
+      Message.warning('请先登录')
+      router.push({
+        name:'login'
+      })
+      return 
+    }
+    next()
+  }
+})
+export default router
